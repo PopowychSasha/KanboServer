7fac2c1845e95cf359e720580ec9e8d7a1467762
Alex <popowuch1104@gmail.com>
2022-04-24 15:01:00 +0300

    add getBoard controller

--- controllers/board.js
+++ controllers/board.js
@@ -22,3 +22,29 @@ exports.createBoard = async (req,res,next)=>{
     })
     .catch((err)=>console.log(err.message))
 }
+
+exports.getBoards = async (req,res,next)=>{
+    const nicknameFromCookie = req.get('Cookie').split('=')[1].split('%')[0];
+
+    const [user] = await Users.findAll({
+        where:{
+            nickname:nicknameFromCookie
+        }
+    })
+
+    const boards = await Boards.findAll({
+        where:{
+            userId:user.id
+        }
+    });
+    const boardsResponce = boards.map(item=>{
+        return {
+            id:item.dataValues.id,
+            name:item.dataValues.name,
+            type:item.dataValues.type,
+            createdAt:item.dataValues.createdAt,
+        }
+    })
+
+    res.json(boardsResponce);
+}
--- routes/board.js
+++ routes/board.js
@@ -3,5 +3,6 @@ const routes = express.Router();
 const boardControllers = require('../controllers/board');
 
 routes.post('/api/board',boardControllers.createBoard);
+routes.get('/api/boards',boardControllers.getBoards);
 
 module.exports = routes;
